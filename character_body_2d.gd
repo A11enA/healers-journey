dc3ea0a17c2c1b41ec87e75845008b8addbf9c64
A11enA <alasiakallen@gmail.com>
2025-10-23 15:13:00 -0500

    init & movement

---
+++ character_body_2d.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+
+const move_speed: float = 100.0
+
+
+
+func _physics_process(delta: float) -> void:
+	var move_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = move_vector * move_speed
+
+	move_and_slide()
