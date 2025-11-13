d2d7d12b597068b5d0fbdad77759091e811a8218
A11enA <alasiakallen@gmail.com>
2025-11-13 15:31:08 -0600

    dungeon added, puzzle made

--- Scripts/puzzle_button.gd
+++ Scripts/puzzle_button.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
-
+signal pushed
+signal unpushed
+var bodies_on_button = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -16,12 +18,19 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	print("something is on top of me")
 	#play Animation
-	$AnimatedSprite2D.play("pushed")
-	#open a door by sending a signal
+	bodies_on_button += 1
+	if body.is_in_group("pushable") or body is Player:
+		if bodies_on_button == 1:
+			$AnimatedSprite2D.play("pushed")
+			#open a door by sending a signal
+			pushed.emit()
 
 
 func _on_body_exited(body: Node2D) -> void:
 	print("nothing is on top of me")
 	#play Animation
-	$AnimatedSprite2D.play("unpushed")
-	#close door
+	bodies_on_button -= 1
+	if bodies_on_button == 0:
+		$AnimatedSprite2D.play("unpushed")
+		#close door
+		unpushed.emit()
