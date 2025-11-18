ff29b301bb8ca05727507b5ceaf183a899e15fe1
A11enA <alasiakallen@gmail.com>
2025-11-18 15:47:21 -0600

    Spring cleaning doors and puzzles
    Did some cleaning up, Added doors and completed dungeon puzzle.

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -1,6 +1,8 @@
 extends StaticBody2D
 
-
+@export var button_presses_needed: int = 1
+#keep track of how many buttons are pressed
+var buttons_pressed: int = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,10 +14,14 @@ func _process(delta: float) -> void:
 
 
 func _on_puzzle_button_pushed() -> void:
-	$AnimatedSprite2D.animation = "open"
-	$CollisionShape2D.set_deferred("disabled", true)
+	buttons_pressed += 1
+	if buttons_pressed == button_presses_needed:
+		$AnimatedSprite2D.animation = "open"
+		$CollisionShape2D.set_deferred("disabled", true)
+		$move.play()
 
 
 func _on_puzzle_button_unpushed() -> void:
-	$AnimatedSprite2D.animation = "closed"
-	$CollisionShape2D.set_deferred("disabled", false)
+	if buttons_pressed != button_presses_needed:
+		$AnimatedSprite2D.animation = "closed"
+		$CollisionShape2D.set_deferred("disabled", false)
